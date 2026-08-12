0222629e025f6f012413f055e5b8df32400a70ea
JLoKloNnIk <artemmomot505@gmail.com>
2026-08-12 20:39:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -586,6 +586,7 @@ async def admin_panel(message: types.Message):
 
 # ---------- Запуск ----------
 async def main():
+    print("DATABASE_URL:", DATABASE_URL)
     await init_db()
     print("Бот запущен...")
     await bot.delete_webhook(drop_pending_updates=True)
